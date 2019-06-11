5cd67bc70654bc8485c224b9f1f273e82f579427
nawrrot <kontakt@piotrnawrocki.pl>
2019-06-11 13:12:44 +0200

    Javadoc changes in xmlWorker class.

--- src/com/rental/utils/XmlWorker.java
+++ src/com/rental/utils/XmlWorker.java
@@ -25,7 +25,6 @@ import java.io.IOException;
  * @author Piotr Nawrocki
  */
 public class XmlWorker {
-
     private static final String USER_NAME_ATT = "user_name";
     private static final String USER_PASSWORD_ATT = "password";
     private static final String PRODUCT_IN_STOCK_ATT = "in_stock";
@@ -36,54 +35,58 @@ public class XmlWorker {
     private static final String PRODUCT_BASE_PATH = "resources/products.xml";
     private static final String USER_TAG_NAME = "user";
     private static final String PRODUCT_TAG_NAME = "product";
-    private Document productsBase;
-    private Document usersBase;
-    NodeList productList;
-    public NodeList userList;
+    static final int MAX_QUANTITY_OF_PRODUCTS = 3;
+    private static Document productsBase;
+    private static Document usersBase;
+    static NodeList productList;
+    public static NodeList userList;
     private Element rootUser;
-    private Element rootElementUser;
-    private DOMSource productSource;
-    private DOMSource usersSource;
-    Element productElement;
+    private static DOMSource productSource;
+    private static DOMSource usersSource;
+    static Element productElement;
+    private static DocumentBuilderFactory documentBuilderFactory;
 
     /**
      * The XmlWorker constructor provides information about all xml, specified initialization parameters.
      */
-
     public XmlWorker() {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+        documentBuilderFactory = DocumentBuilderFactory
             .newInstance();
         try {
             DocumentBuilder documentBuilder = documentBuilderFactory
                 .newDocumentBuilder();
-            this.productsBase = documentBuilder.parse(PRODUCT_BASE_PATH);
-            this.usersBase = documentBuilder.parse(USER_BASE_PATH);
+            productsBase = documentBuilder.parse(PRODUCT_BASE_PATH);
+            usersBase = documentBuilder.parse(USER_BASE_PATH);
+            productList = productsBase.getElementsByTagName(PRODUCT_TAG_NAME);
+            userList = usersBase.getElementsByTagName(USER_TAG_NAME);
+            this.rootUser = usersBase.getDocumentElement();
+            productSource = new DOMSource(productsBase);
+            usersSource = new DOMSource(usersBase);
+            documentBuilderFactory.setIgnoringElementContentWhitespace(true);
         } catch (ParserConfigurationException e) {
             throw new RuntimeException(e);
         } catch (SAXException | IOException e) {
             e.printStackTrace();
         }
-        this.productList = productsBase.getElementsByTagName(PRODUCT_TAG_NAME);
-        this.userList = usersBase.getElementsByTagName(USER_TAG_NAME);
-        this.rootUser = usersBase.getDocumentElement();
-        this.rootElementUser = usersBase.getDocumentElement();
-        this.productSource = new DOMSource(this.productsBase);
-        this.usersSource = new DOMSource(this.usersBase);
     }
 
     /**
      * Approves all changes in the products.xml file, made at the stage of application operation.
      */
-    private void saveChangesInXmlProductsFiles() {
+    private static void saveChangesInXmlProductsFiles() {
         try {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "all");
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "all");
             transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-16");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
             StreamResult productsResult = new StreamResult(PRODUCT_BASE_PATH);
             transformer.transform(productSource, productsResult);
+
         } catch (TransformerException e) {
             e.printStackTrace();
         }
@@ -92,16 +95,20 @@ public class XmlWorker {
     /**
      * Approves all changes in the users.xml file, made at the stage of application operation.
      */
-    private void saveChangesInXmlUsersFiles() {
+    private static void saveChangesInXmlUsersFiles() {
         try {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "all");
             transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "all");
             transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
             Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-16");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
             StreamResult usersResult = new StreamResult(USER_BASE_PATH);
             transformer.transform(usersSource, usersResult);
+            documentBuilderFactory.setIgnoringElementContentWhitespace(true);
         } catch (TransformerException e) {
             e.printStackTrace();
         }
@@ -128,65 +135,80 @@ public class XmlWorker {
     /**
      * Gets and enters new changes attribute of products.xml file.
      *
-     * @param userInput data entered by the user to get the appropriate selection of values from product.xml file.
+     * @param productId data entered by the user to get the appropriate selection of values from product.xml file.
      */
-    public void takeProductOutOfStock(int userInput) {
-        String getInStock = productList.item(userInput).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).getNodeValue();
+    private static void takeProductFromStock(int productId) {
+        String getInStock = productList.item(productId).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).getNodeValue();
         int getNewInStock = Integer.parseInt(getInStock);
         getNewInStock--;
         if (getNewInStock < 0) {
             Communicator.outOfStock();
             System.exit(0);
         }
-        productList.item(userInput).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).setNodeValue(Integer.toString(getNewInStock));
+        productList.item(productId).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).setNodeValue(Integer.toString(getNewInStock));
+        saveChangesInXmlProductsFiles();
     }
 
     /**
      * Gets a string including value of parameter from users.xml file.<p>
      * In the case of a positive implementation, the data is entered into the appropriate xml file.
      *
-     * @param userInputSelected data entered by the user to get and change value in product.xml file
+     * @param productId data entered by the user to get and change value in product.xml file
      */
-    public void returnProductOnStock(int userInputSelected) {
-        String getInStock = productList.item(userInputSelected).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).getNodeValue();
+    private static void returnProductOnStock(int productId) {
+        String getInStock = productList.item(productId).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).getNodeValue();
         int getNewInStock = Integer.parseInt(getInStock);
         getNewInStock++;
-        productList.item(userInputSelected).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).setNodeValue(Integer.toString(getNewInStock));
-        saveChangesInXmlUsersFiles();
+        productList.item(productId).getAttributes().getNamedItem(PRODUCT_IN_STOCK_ATT).setNodeValue(Integer.toString(getNewInStock));
         saveChangesInXmlProductsFiles();
     }
 
     /**
-     * Changes the name of the attribute in the users.xml file to null.
+     * Responsible for attaching an appropriate selection to the users.xml file.<p>
      * Additionally, it checks and compares the user input and xml file if the value can be added.
-     * In the case of a positive implementation, the data is entered into the appropriate xml file.
      *
-     * @param login reference to the object created in the RentalProcessor class
+     * @param productId data entered by the user to get the appropriate selection of values to users.xml file.
      */
-    public void deleteProductFromUserBase(Login login) {
+    public static void enterProductNameToUserBase(int productId) {
+        String nameOfRentedProduct = productList.item(productId).getAttributes().getNamedItem(PRODUCT_NAME_ATT).getNodeValue();
         for (int i = 0; i < userList.getLength(); i++) {
             Node tryToFindUser = userList.item(i);
-            if (tryToFindUser.getAttributes().getNamedItem(USER_NAME_ATT).getTextContent().equalsIgnoreCase(login.getNameOfLoggedUser())) {
-                ((Element) tryToFindUser).setAttribute(RENTED_PRODUCT_ATT, null);
+            if (tryToFindUser.getAttributes().getNamedItem(USER_NAME_ATT).getTextContent().equalsIgnoreCase(Login.nameOfLoggedUser)) {
+                NodeList getChildNodesFromUser = usersBase.getElementsByTagName(USER_TAG_NAME).item(i).getChildNodes();
+                System.out.println(getChildNodesFromUser.getLength());
+                if (MAX_QUANTITY_OF_PRODUCTS > getChildNodesFromUser.getLength()) {
+                    Element products = usersBase.createElement(RENTED_PRODUCT_ATT);
+                    tryToFindUser.appendChild(products);
+                    Attr attr = usersBase.createAttribute(PRODUCT_NAME_ATT);
+                    attr.setValue(nameOfRentedProduct);
+                    products.setAttributeNode(attr);
+                    takeProductFromStock(productId);
+                    saveChangesInXmlUsersFiles();
+                } else {
+                    Communicator.rentRequirement();
+                    break;
+                }
             }
         }
-        saveChangesInXmlUsersFiles();
-        saveChangesInXmlProductsFiles();
     }
 
     /**
-     * Responsible for attaching an appropriate selection to the xml file.<p>
-     * Additionally, it checks and compares the user input and xml file if the value can be added.
-     *
-     * @param userInput data entered by the user to get the appropriate selection of values ti users.xml file.
-     * @param login     reference to the object created in the RentalProcessor class
+     * Responsible for returning an appropriate selection from the users.xml and products.xml files.
      */
-    public void enterProductNameToUserBase(int userInput, Login login) {
-        String nameOfRentedProduct = productList.item(userInput).getAttributes().getNamedItem(PRODUCT_NAME_ATT).getNodeValue();
+    public static void returnRentedProduct() {
         for (int i = 0; i < userList.getLength(); i++) {
             Node tryToFindUser = userList.item(i);
-            if (tryToFindUser.getAttributes().getNamedItem(USER_NAME_ATT).getTextContent().equalsIgnoreCase(login.getNameOfLoggedUser())) {
-                ((Element) tryToFindUser).setAttribute(RENTED_PRODUCT_ATT, nameOfRentedProduct);
+            if (tryToFindUser.getAttributes().getNamedItem(USER_NAME_ATT).getTextContent().equalsIgnoreCase(Login.nameOfLoggedUser)) {
+                NodeList getChildNodesFromUser = usersBase.getElementsByTagName(USER_TAG_NAME).item(i).getChildNodes();
+                for (int j = 0; j < getChildNodesFromUser.getLength(); j++) {
+                    System.out.println(j + "." + getChildNodesFromUser.item(j).getAttributes().getNamedItem(PRODUCT_NAME_ATT).getTextContent());
+                }
+                Node getNodeLoggedUser = usersBase.getElementsByTagName(USER_TAG_NAME).item(i);
+                int productId = Communicator.idProductToReturn();
+                Node getSelectedChildNode = getNodeLoggedUser.getChildNodes().item(productId);
+                getNodeLoggedUser.removeChild(getSelectedChildNode);
+                returnProductOnStock(productId);
+                saveChangesInXmlUsersFiles();
             }
         }
     }
@@ -198,20 +220,20 @@ public class XmlWorker {
      * @param idLengthAsString numerical value(String) of the attribute ID
      */
     public void createUserElementsAndValuesInBase(String idLengthAsString) {
-        Element account = usersBase.createElement(USER_NAME_ATT);
+        Element users = usersBase.createElement(USER_TAG_NAME);
         Attr attr = usersBase.createAttribute(USER_ID_ATT);
         attr.setValue(idLengthAsString);
-        rootElementUser.appendChild(account);
-        account.setAttributeNode(attr);
+        rootUser.appendChild(users);
+        users.setAttributeNode(attr);
         Attr attrLogin = usersBase.createAttribute(USER_NAME_ATT);
         attrLogin.setValue(Communicator.enterLoginField());
-        rootElementUser.appendChild(account);
-        account.setAttributeNode(attrLogin);
+        rootUser.appendChild(users);
+        users.setAttributeNode(attrLogin);
         Attr attrPassword = usersBase.createAttribute(USER_PASSWORD_ATT);
         attrPassword.setValue(Communicator.enterPasswordField());
-        rootElementUser.appendChild(account);
-        account.setAttributeNode(attrPassword);
-        rootUser.appendChild(account);
+        rootUser.appendChild(users);
+        users.setAttributeNode(attrPassword);
+        rootUser.appendChild(users);
         saveChangesInXmlUsersFiles();
     }
 }
